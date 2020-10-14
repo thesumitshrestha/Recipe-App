717310d795a79a510a774f7136a2cfa3eb8bf300
thesumitshrestha <summitshrestha8@gmail.com>
2020-10-14 15:11:59 +0545

    first commit

---
+++ src/Recipe.js
@@ -0,0 +1,19 @@
+import App from "./App";
+import React from "react";
+
+const Recipe = ({title, calories, image, ingredients}) => {
+  return (
+    <div>
+      <h1> {title}</h1>
+      <ol>
+        {ingredients.map(ingredient => (
+          <li> {ingredient.text}</li>
+        ))}
+      </ol>
+      <p> {calories}</p>
+      <img src={image} alt=""/>
+    </div>
+  );
+}
+
+export default Recipe;
